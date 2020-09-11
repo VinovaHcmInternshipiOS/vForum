c6e31848813b32c3d4b0994b1326586a2197704f
theironicpony <trucdong.txtv@gmail.com>
2020-09-11 15:05:50 +0700

    Create login screen

--- vForum/Controller/LoginController.swift
+++ vForum/Controller/LoginController.swift
@@ -2,9 +2,15 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    @IBOutlet weak var Logo: UIImageView!
+    
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Username.layer.cornerRadius = 7
+        Password.layer.cornerRadius = 7
     }
 
 

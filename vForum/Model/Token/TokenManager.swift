d571eddbe6c7872efdeb2920a489df63699150e1
Phuc <phucgo240699@gmail.com>
2020-09-15 16:24:49 +0700

    Change TokenManager class

--- vForum/Model/Token/TokenManager.swift
+++ vForum/Model/Token/TokenManager.swift
@@ -12,74 +12,19 @@ import Alamofire
 class TokenManager {
     static var shared = TokenManager()
     
-    func getTokenFromLocal() -> String? {
-        let token = UserDefaults.standard.object(forKey: "token") as? String
-        return token
+    func getAccessTokenFromLocal() -> String? {
+        let accessToken = UserDefaults.standard.object(forKey: "accessToken") as? String
+        return accessToken
+    }
+    func getRefreshTokenFromLocal() -> String? {
+        let refreshToken = UserDefaults.standard.object(forKey: "refreshToken") as? String
+        return refreshToken
     }
     
-    func isValidToken(url: String, token: String) -> Bool {
-        
-        let rawUrl = URL(string: url)
-        
-        // Un-wrap
-        guard let url = rawUrl else {
-            return false
-        }
-        
-        // Defind semaphore
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        AF.request(url, method: .post, parameters: ["token": token], encoding: JSONEncoding.default, headers: nil).response {
-            response in
-            // valid token -> true
-            
-            // Invalid token
-            semaphore.signal()
-        }
-        
-        semaphore.wait()
-        
-        return false
+    func updateAccessTokenIntoLocal(token: String) {
+        UserDefaults.standard.set(token, forKey: "accessToken")
     }
-    
-    func getTokenFromServer(url: String, params: [String: Any]) -> String? {
-        // Properties
-        var token: String?
-        let rawUrl = URL(string: url)
-        
-        // Un-wrap
-        guard let url = rawUrl else {
-            return nil
-        }
-        
-        // Define semaphore
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        // Request
-        AF.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).response{
-            response in
-            // Get data from response of server
-            if let data = response.data {
-                do {
-                    // Decode data to TokenResponse type
-                    let tokenResponse = try JSONDecoder().decode(TokenResponse.self, from: data)
-                    
-                    token = tokenResponse.data
-                    semaphore.signal() // semaphore signal
-                } catch {
-                    print(error)
-                    semaphore.signal() // semaphore signal
-                }
-            }
-            
-        }
-        
-        semaphore.wait() // semaphore wait response of API
-        
-        return token
-    }
-    
-    func updateTokenIntoLocal(token: String) {
-        UserDefaults.standard.set(token, forKey: "token")
+    func updateRefreshTokenIntoLocal(token: String) {
+        UserDefaults.standard.set(token, forKey: "refreshToken")
     }
 }

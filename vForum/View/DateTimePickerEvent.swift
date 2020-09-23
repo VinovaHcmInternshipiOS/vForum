1b0c2f201f41e5f2e1d9092e944fb9f6db837b53
phucgo240699 <phucgo240699@gmail.com>
2020-09-23 10:45:26 +0700

    Create two files: ListEventController, DateTimePickerEvent

---
+++ vForum/View/DateTimePickerEvent.swift
@@ -0,0 +1,9 @@
+//
+//  DateTimePickerEvent.swift
+//  vForum
+//
+//  Created by Phúc Lý on 9/23/20.
+//  Copyright © 2020 vinova.internship. All rights reserved.
+//
+
+import Foundation
---
+++ vForum/View/ListEventController.swift
@@ -0,0 +1,9 @@
+//
+//  ListEventController.swift
+//  vForum
+//
+//  Created by Phúc Lý on 9/23/20.
+//  Copyright © 2020 vinova.internship. All rights reserved.
+//
+
+import Foundation

d53b8ec25f82bc6ac74664a43e00e0388acc606b
phucgo240699 <phucgo240699@gmail.com>
2020-09-23 10:50:00 +0700

    Created sorting bar

--- vForum/View/ListEventController.swift
+++ vForum/View/ListEventController.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+import UIKit
+import DropDown
+
+class ListEventController: UIViewController {
+    
+    var sortTypeBtn: UIButton?
+    var sortDateBtn: UIButton?
+    var tableView: UITableView?
+    var navBarHeight: CGFloat = 70.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        initializeSortTypeBtn(&sortTypeBtn)
+        initializeSortDateBtn(&sortDateBtn)
+        
+    }
+    
+    func initializeSortTypeBtn(_ sortType: inout UIButton?) {
+        
+        sortType = UIButton(frame: CGRect(x: 0, y: navBarHeight, width: view.bounds.width * 0.4, height: view.bounds.height * 0.1))
+        
+        guard let sortType = sortType else {
+            return
+        }
+        
+        sortType.setTitle("Sorting", for: .normal)
+        sortType.setTitleColor(UIColor(red: 171/255, green: 169/255, blue: 195/255, alpha: 1.0), for: .normal)
+        sortType.addTarget(self, action: #selector(sortTypeBtnPressed(_:)), for: .touchUpInside)
+        
+        view.addSubview(sortType)
+    }
+    
+    func initializeSortDateBtn(_ sortDate: inout UIButton?) {
+        
+        sortDate = UIButton(frame: CGRect(x: view.bounds.width * 0.5, y: navBarHeight, width: view.bounds.width * 0.4, height: view.bounds.height * 0.1))// getSortDate(self.view, "15 April")
+        
+        guard let sortDate = sortDate else {
+            return
+        }
+        
+        sortDate.setTitle("15 April", for: .normal)
+        sortDate.setTitleColor(UIColor(red: 171/255, green: 169/255, blue: 195/255, alpha: 1.0), for: .normal)
+        sortDate.addTarget(self, action: #selector(sortDateBtnPressed(_:)), for: .touchUpInside)
+        
+        view.addSubview(sortDate)
+    }
+    
+    @objc func sortDateBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    @objc func sortTypeBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+}
